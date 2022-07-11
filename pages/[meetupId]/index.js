c5b6ab304c0634ff03152ce26326f230e265c5ed
steven <8312553+sphred5@users.noreply.github.com>
2022-07-11 15:11:03 -0700

    added meetup detail page

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,13 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail.js";
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://pocket-syndicated-images.s3.amazonaws.com/5ecea0640d986.jpg"
+      title="A meetup"
+      address="123 fake st"
+      description="a meetup description"
+    />
+  );
+};
+
+export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,32 @@
- 
+import MeetupList from "../components/meetups/MeetupList.js";
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A first meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1024px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "some address 12345",
+    description: "meetup description",
+  },
+  {
+    id: "m2",
+    title: "A second meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1024px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "some address 12345",
+    description: "meetup description",
+  },
+  {
+    id: "m3",
+    title: "A third meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1024px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "some address 12345",
+    description: "meetup description",
+  },
+];
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+};
+
+export default HomePage;
