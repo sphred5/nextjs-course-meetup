bef74ae8b2d7231a6b7e7146a68c8c8658766b70
steven <8312553+sphred5@users.noreply.github.com>
2022-07-11 16:15:50 -0700

    started database connection

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,13 +1,58 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail.js";
-const MeetupDetails = () => {
+const MeetupDetails = ({ meetupData }) => {
+  const { image, title, address, description } = meetupData;
+
   return (
     <MeetupDetail
-      image="https://pocket-syndicated-images.s3.amazonaws.com/5ecea0640d986.jpg"
-      title="A meetup"
-      address="123 fake st"
-      description="a meetup description"
+      image={image}
+      title={title}
+      address={address}
+      description={description}
     />
   );
 };
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+      {
+        params: {
+          meetupId: "m3",
+        },
+      },
+      {
+        params: {
+          meetupId: "m4",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  return {
+    props: {
+      meetupData: {
+        id: "m1",
+        image:
+          "https://pocket-syndicated-images.s3.amazonaws.com/5ecea0640d986.jpg",
+        title: "A Meetup Title",
+        address: "123 fake st",
+        description: "a meetup description",
+      },
+    },
+  };
+}
 export default MeetupDetails;
